5323809d70d0de84113cb2ed6705e72305b50af6
Melike Çiğdem <cigdemmelike37@gmail.com>
2025-01-13 19:22:31 +0300

    Revert "Adding to cart with redux-toolkit"
    This reverts commit 2937864122bb0debe452f6da55faea4e552d418a.

--- me-like/src/app/store/store.js
+++
@@ -1,6 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import pointOfSellReducer from "./features/pointOfSell/pointOfSellSlice";
-
-export const store = configureStore({
-  reducer: { pointOfSell: pointOfSellReducer },
-});
